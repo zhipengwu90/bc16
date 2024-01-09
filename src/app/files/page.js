1ff60dd9956a71c99818a21bf988957c6977b7f8
zhipengwu90 <zhipeng.wu@itas.ca>
2024-01-08 16:29:33 -0800

    added link to open pdf

--- src/app/files/page.js
+++ src/app/files/page.js
@@ -8,6 +8,25 @@ const File = async ({ searchParams }) => {
   const fileName = searchParams.fileName;
   const folderName = searchParams.folderName;
 
+  const format6e = folderName == "6eresultocr";
+  const format4c = folderName == "4cresultocr";
+  const format4h = folderName == "4hresultocr";
+  const format5a = folderName == "5aresultocr";
+  const format7e = folderName == "7eresultocr";
+  const pdfFolderName = format6e
+    ? "6etest"
+    : format4c
+    ? "4ctest"
+    : format4h
+    ? "4hrun"
+    : format5a
+    ? "5atest"
+    : format7e
+    ? "7etest"
+    : "";
+
+
+
   try {
     const filePath =
       process.cwd() + `/src/app/bc16Data/${folderName}/${fileName}`;
@@ -23,6 +42,14 @@ const File = async ({ searchParams }) => {
           Back
         </Link> */}
         <h5>File Name: {fileName.replace(/_/g, " ").replace(".json", "")}</h5>
+        <Link
+          className={styles.linkStyle}
+          rel="noopener noreferrer"
+          target="_blank"
+          href={`https://bc16teststorage.blob.core.windows.net/${pdfFolderName}/${fileName.replace(".json", ".pdf")}`}
+        >
+          PDF Version
+        </Link>
         <div className={styles.container}>
           <FormRender folderName={folderName} items={jsonData} />
         </div>
