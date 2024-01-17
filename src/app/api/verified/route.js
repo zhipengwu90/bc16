4ef3c537b4caea8274e61af0d25fd9dac93130b2
zhipengwu90 <zhipeng.wu@itas.ca>
2024-01-16 22:20:57 -0800

    remove exits

--- src/app/api/verified/route.js
+++ src/app/api/verified/route.js
@@ -12,49 +12,49 @@ export async function POST(request) {
     // Generate a blob name based on the folder name
     const blobName = `${dataJson.folderName}.json`;
 
-    const blockBlobClient = await containerClient.getBlockBlobClient(blobName);
+    const blockBlobClient = containerClient.getBlockBlobClient(blobName);
     console.log("blockBlobClient" + blockBlobClient);
     // Check if the blob already exists
 
-    const blobExists = await blockBlobClient.exists();
-    console.log("blobExists" + blobExists);
+    // const blobExists = await blockBlobClient.exists();
+    // console.log("blobExists" + blobExists);
 
-    if (blobExists) {
-      // If blob exists, download the existing JSON data
-      const existingData = await blockBlobClient.downloadToBuffer();
-      console.log("existingData" + existingData);
-      const existingJson = existingData.toString();
+    // if (blobExists) {
+    //   // If blob exists, download the existing JSON data
+    //   const existingData = await blockBlobClient.downloadToBuffer();
+    //   console.log("existingData" + existingData);
+    //   const existingJson = existingData.toString();
 
-      // Parse the existing JSON data
-      let existingJsonArray;
-      try {
-        existingJsonArray = JSON.parse(existingJson);
-      } catch (error) {
-        console.error(`Error parsing existing JSON: ${error}`);
-        return;
-      }
+    //   // Parse the existing JSON data
+    //   let existingJsonArray;
+    //   try {
+    //     existingJsonArray = JSON.parse(existingJson);
+    //   } catch (error) {
+    //     console.error(`Error parsing existing JSON: ${error}`);
+    //     return;
+    //   }
 
-      // Append new data to existing JSON array
-      existingJsonArray = [...existingJsonArray, ...data];
-      console.log(existingJsonArray);
-      // Convert the updated data to JSON string
-      const updatedJsonData = JSON.stringify(existingJsonArray, null, 2);
+    //   // Append new data to existing JSON array
+    //   existingJsonArray = [...existingJsonArray, ...data];
+    //   console.log(existingJsonArray);
+    //   // Convert the updated data to JSON string
+    //   const updatedJsonData = JSON.stringify(existingJsonArray, null, 2);
 
-      // Upload the updated JSON data to the blob
-      await blockBlobClient.upload(updatedJsonData, updatedJsonData.length, {
-        blobHTTPHeaders: { blobContentType: "application/json" },
-      });
-      return new Response("Success", { status: 200 });
-    } else {
-      // If blob doesn't exist, create a new JSON array
-      const jsonData = JSON.stringify(data, null, 2);
+    //   // Upload the updated JSON data to the blob
+    //   await blockBlobClient.upload(updatedJsonData, updatedJsonData.length, {
+    //     blobHTTPHeaders: { blobContentType: "application/json" },
+    //   });
+    //   return new Response("Success", { status: 200 });
+    // } else {
+    // If blob doesn't exist, create a new JSON array
+    const jsonData = JSON.stringify(data, null, 2);
 
-      //     // Upload the new JSON array to the blob
-      await blockBlobClient.upload(jsonData, jsonData.length, {
-        blobHTTPHeaders: { blobContentType: "application/json" },
-      });
-      return new Response("Success", { status: 200 });
-    }
+    //     // Upload the new JSON array to the blob
+    await blockBlobClient.upload(jsonData, jsonData.length, {
+      blobHTTPHeaders: { blobContentType: "application/json" },
+    });
+    return new Response("Success", { status: 200 });
+    // }
   } catch (error) {
     return new Response(error.message, { status: 500 });
   }
