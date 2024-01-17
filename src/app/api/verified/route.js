e8911b0df00ead6bb6b8cb9efaf491f1fd2eb6be
zhipengwu90 <zhipeng.wu@itas.ca>
2024-01-16 22:11:39 -0800

    change to api route

--- src/app/api/verified/route.js
+++ src/app/api/verified/route.js
@@ -6,23 +6,23 @@ export async function POST(request) {
     const SAS_URL = process.env.NEXT_PUBLIC_SAS_URL;
     console.log(SAS_URL);
     const blobService = new BlobServiceClient(SAS_URL);
-    console.log(blobService);
+    console.log("url" + blobService);
     const containerClient = blobService.getContainerClient("verified");
-    console.log(containerClient);
+    console.log("containerClient" + containerClient);
     // Generate a blob name based on the folder name
     const blobName = `${dataJson.folderName}.json`;
 
     const blockBlobClient = containerClient.getBlockBlobClient(blobName);
-    console.log(blockBlobClient);
+    console.log("blockBlobClient" + blockBlobClient);
     // Check if the blob already exists
 
     const blobExists = await blockBlobClient.exists();
-    console.log(blobExists);
+    console.log("blobExists" + blobExists);
 
     if (blobExists) {
       // If blob exists, download the existing JSON data
       const existingData = await blockBlobClient.downloadToBuffer();
-      console.log(existingData);
+      console.log("existingData" + existingData);
       const existingJson = existingData.toString();
 
       // Parse the existing JSON data
@@ -41,18 +41,18 @@ export async function POST(request) {
       const updatedJsonData = JSON.stringify(existingJsonArray, null, 2);
 
       // Upload the updated JSON data to the blob
-      await blockBlobClient.upload(updatedJsonData, updatedJsonData.length, {
-        blobHTTPHeaders: { blobContentType: "application/json" },
-      });
+      //   await blockBlobClient.upload(updatedJsonData, updatedJsonData.length, {
+      //     blobHTTPHeaders: { blobContentType: "application/json" },
+      //   });
       return new Response("Success", { status: 200 });
     } else {
       // If blob doesn't exist, create a new JSON array
       const jsonData = JSON.stringify(data, null, 2);
 
       //     // Upload the new JSON array to the blob
-      await blockBlobClient.upload(jsonData, jsonData.length, {
-        blobHTTPHeaders: { blobContentType: "application/json" },
-      });
+      //   await blockBlobClient.upload(jsonData, jsonData.length, {
+      //     blobHTTPHeaders: { blobContentType: "application/json" },
+      //   });
       return new Response("Success", { status: 200 });
     }
   } catch (error) {
