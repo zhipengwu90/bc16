f330da6587f48e5a1b515f447593c5bd562eef70
zhipengwu90 <zhipeng.wu@itas.ca>
2024-01-17 10:17:34 -0800

    remove standalone

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ const nextConfig = {
 module.exports = nextConfig;
 
 module.exports = {
-  output: "standalone",
+
 
   env: {
     NEXT_PUBLIC_SAS_URL: process.env.NEXT_PUBLIC_SAS_URL,
--- src/app/api/verified/route.js
+++ src/app/api/verified/route.js
@@ -17,53 +17,53 @@ export async function POST(request) {
     console.log("blockBlobClient" + blockBlobClient);
     // Check if the blob already exists
 
-    // const blobExists = await blockBlobClient.exists();
-    // console.log("blobExists" + blobExists);
+    const blobExists = await blockBlobClient.exists();
+    console.log("blobExists" + blobExists);
 
-    // if (blobExists) {
-    //   // If blob exists, download the existing JSON data
-    //   const existingData = await blockBlobClient.downloadToBuffer();
-    //   console.log("existingData" + existingData);
-    //   const existingJson = existingData.toString();
+    if (blobExists) {
+      // If blob exists, download the existing JSON data
+      const existingData = await blockBlobClient.downloadToBuffer();
+      console.log("existingData" + existingData);
+      const existingJson = existingData.toString();
 
-    //   // Parse the existing JSON data
-    //   let existingJsonArray;
-    //   try {
-    //     existingJsonArray = JSON.parse(existingJson);
-    //   } catch (error) {
-    //     console.error(`Error parsing existing JSON: ${error}`);
-    //     return;
-    //   }
+      // Parse the existing JSON data
+      let existingJsonArray;
+      try {
+        existingJsonArray = JSON.parse(existingJson);
+      } catch (error) {
+        console.error(`Error parsing existing JSON: ${error}`);
+        return;
+      }
 
-    //   // Append new data to existing JSON array
-    //   existingJsonArray = [...existingJsonArray, ...data];
-    //   console.log(existingJsonArray);
-    //   // Convert the updated data to JSON string
-    //   const updatedJsonData = JSON.stringify(existingJsonArray, null, 2);
+      // Append new data to existing JSON array
+      existingJsonArray = [...existingJsonArray, ...data];
+      console.log(existingJsonArray);
+      // Convert the updated data to JSON string
+      const updatedJsonData = JSON.stringify(existingJsonArray, null, 2);
 
-    //   // Upload the updated JSON data to the blob
-    //   await blockBlobClient.upload(updatedJsonData, updatedJsonData.length, {
-    //     blobHTTPHeaders: { blobContentType: "application/json" },
-    //   });
-    //   return new Response("Success", { status: 200 });
-    // } else {
-    // If blob doesn't exist, create a new JSON array
-    const jsonData = JSON.stringify(data, null, 2);
-    console.log("jsonData" + jsonData);
-    //     // Upload the new JSON array to the blob
-    try {
-      //   await blockBlobClient.upload(jsonData, jsonData.length, {
-      //     blobHTTPHeaders: { blobContentType: "application/json" },
-      //   });
+      // Upload the updated JSON data to the blob
+      await blockBlobClient.upload(updatedJsonData, updatedJsonData.length, {
+        blobHTTPHeaders: { blobContentType: "application/json" },
+      });
+      return new Response("Success", { status: 200 });
+    } else {
+      // If blob doesn't exist, create a new JSON array
+      const jsonData = JSON.stringify(data, null, 2);
+      console.log("jsonData" + jsonData);
+      //     // Upload the new JSON array to the blob
+      try {
+        //   await blockBlobClient.upload(jsonData, jsonData.length, {
+        //     blobHTTPHeaders: { blobContentType: "application/json" },
+        //   });
 
-      await blockBlobClient.upload(jsonData, jsonData.length);
-      console.log("uploading");
-    } catch (error) {
-      console.error("Caught an uploading error:", error);
-      return new Response(error.message, { status: 500 });
+        await blockBlobClient.upload(jsonData, jsonData.length);
+        console.log("uploading");
+      } catch (error) {
+        console.error("Caught an uploading error:", error);
+        return new Response(error.message, { status: 500 });
+      }
+      return new Response("Success", { status: 200 });
     }
-    return new Response("Success", { status: 200 });
-    // }
   } catch (error) {
     console.error("Caught an outside error:", error);
     return new Response(error.message, { status: 500 });
