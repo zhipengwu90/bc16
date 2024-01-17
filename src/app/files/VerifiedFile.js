984ebdd4e12f9667f94ab1a39094f1fb2e140ea6
zhipengwu90 <zhipeng.wu@itas.ca>
2024-01-17 15:45:58 -0800

    add verified

--- src/app/files/VerifiedFile.js
+++ src/app/files/VerifiedFile.js
@@ -2,12 +2,13 @@
 import { useState } from "react";
 // import { verified } from "./verified";
 // import { useFormState } from "react-dom";
-import style from "./VerifiedFile.module.css";
+import styles from "./VerifiedFile.module.css";
 const VerifiedFile = ({ folderName, fileName, verified }) => {
   //   const [state, formAction] = useFormState(verified, {
   //     status: null,
   //     message: null,
   //   });
+  const [isToggled, setIsToggled] = useState(false);
   const [isSuccess, setIsSuccess] = useState(false);
   const onClickHandler = async (e) => {
     const submitData = {
@@ -27,28 +28,78 @@ const VerifiedFile = ({ folderName, fileName, verified }) => {
       throw new Error(Response.statusText);
     } else {
       console.log("Success");
+      alert("Success");
       setIsSuccess(true);
+      setIsToggled(false);
     }
   };
-
+  const onPasswordHandler = (e) => {
+    const password = document.getElementById("password").value;
+    if (password === "1234") {
+      onClickHandler(e);
+ 
+    } else {
+      alert("Wrong Password");
+    }
+  };
   return (
-    <form
-      onSubmit={(e) => {
-        e.preventDefault();
-        onClickHandler(e);
-      }}
-    >
-      <input
-        type="hidden"
-        id="folderName"
-        name="folderName"
-        value={folderName}
-      />
-      <input type="hidden" id="fileName" name="fileName" value={fileName} />
-      <input type="hidden" id="verified" name="verified" value="true" />
-      {isSuccess && <p className={style.success}>Submit Successfully</p>}
-      {!isSuccess && !verified && <button type="submit">Verified File</button>}
-    </form>
+    <>
+      {isToggled && (
+        <>
+          <div
+            className={styles.backdrop}
+            onClick={() => setIsToggled(!isToggled)}
+          ></div>
+          <form
+            className={styles.errorWindow}
+            onSubmit={(e) => {
+              e.preventDefault();
+              onPasswordHandler(e);
+            }}
+          >
+            <div className={styles.passwordWrap}>
+              <label htmlFor="password">Password</label>
+
+              <input type="password" id="password" name="password" />
+            </div>
+
+            <input
+              type="hidden"
+              id="folderName"
+              name="folderName"
+              value={folderName}
+            />
+            <input
+              type="hidden"
+              id="fileName"
+              name="fileName"
+              value={fileName}
+            />
+            <input type="hidden" id="verified" name="verified" value="true" />
+            {isSuccess && <p className={styles.success}>Submit Successfully</p>}
+            <div className={styles.buttonWrapper}>
+              <button className={styles.submit} type="submit">
+                Submit
+              </button>
+              <button
+                className={styles.cancel}
+                onClick={() => {
+                  setIsToggled(false);
+                }}
+              >
+                Cancel
+              </button>
+            </div>
+          </form>
+        </>
+      )}
+
+      {!isSuccess && !verified && (
+        <button type="submit" onClick={() => setIsToggled(true)}>
+          Verified File
+        </button>
+      )}
+    </>
   );
 };
 
